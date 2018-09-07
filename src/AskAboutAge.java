6c8b70540165d3dceb4dac8a2011963c17d4f672
John Foley <jfoley@cs.umass.edu>
2018-09-07 12:45:47 -0400

    Examples from first lecture

---
+++ src/AskAboutAge.java
@@ -0,0 +1,12 @@
+public class AskAboutAge {
+	public static void main(String[] args) {
+		int adultAge = 28;
+		double childAge = 3.5;
+		char letter = 'J';
+		String childName = "Little Kid";
+		System.out.println(childName + " will tell you that they are " + childAge + ", because they are specific.");
+		System.out.print("An older person, ");
+		System.out.print("(let's call them "+letter+"), ");
+		System.out.println("will tell you they are "+adultAge + ".");
+	}
+}
---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	// We can make a new function like python's print.
+	public static void print(String message) {
+		System.out.println(message);
+	}
+	public static void main(String[] args) {
+		print("I can't believe this works!");
+	}
+}
